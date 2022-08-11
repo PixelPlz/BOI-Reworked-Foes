ee1ceb182e66153d5369d77df7abad51cb21d5a4
PixelPlz <101890162+PixelPlz@users.noreply.github.com>
2022-08-11 23:08:42 +0200

    Add files via upload

---
+++ scripts/selflessKnight.lua
@@ -0,0 +1,32 @@
+local mod = BetterMonsters
+local game = Game()
+
+local Settings = {
+	SpeedMultiplier = 0.95,
+	Cooldown = 30,
+	ShotSpeed = 10
+}
+
+
+
+function mod:selflessKnightUpdate(entity)
+	if entity.Variant == 1 then
+		entity.Velocity = entity.Velocity * Settings.SpeedMultiplier
+
+		if entity.ProjectileCooldown <= 0 then
+			if entity.State == NpcState.STATE_ATTACK and entity.FrameCount > 30 and not entity:IsDead() then
+				local params = ProjectileParams()
+				params.Variant = ProjectileVariant.PROJECTILE_TEAR
+				params.FallingAccelModifier = 0.175
+
+				entity:FireProjectiles(entity.Position, -entity.Velocity:Normalized() * Settings.ShotSpeed, 0, params)
+				entity.ProjectileCooldown = Settings.Cooldown
+				SFXManager():Play(SoundEffect.SOUND_TEARS_FIRE, 0.5)
+			end
+
+		else
+			entity.ProjectileCooldown = entity.ProjectileCooldown - 1
+		end
+	end
+end
+mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, mod.selflessKnightUpdate, EntityType.ENTITY_KNIGHT)
